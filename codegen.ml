447d2776763c609d04d00b50f8b46119b7c0f0f7
mem2318 <mem2318@columbia.edu>
2018-03-19 16:37:21 -0400

    removed types we dont have

--- codegen.ml
+++ codegen.ml
@@ -27,8 +27,6 @@ let translate (globals, functions) =
   let i32_t      = L.i32_type    context
   and i8_t       = L.i8_type     context
   and i1_t       = L.i1_type     context
-  and float_t    = L.double_type context
-  and void_t     = L.void_type   context 
   (* Create an LLVM module -- this is a "container" into which we'll 
      generate actual code *)
   and the_module = L.create_module context "mus" in
@@ -37,8 +35,6 @@ let translate (globals, functions) =
   let ltype_of_typ = function
       A.Int   -> i32_t
     | A.Bool  -> i1_t
-    | A.Float -> float_t
-    | A.Void  -> void_t
   in
 
   (* Declare each global variable; remember its value in a map *)
