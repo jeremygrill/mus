5cd729d26fa798c3f32a56a31fba35c53b8a2448
mem2318 <mem2318@columbia.edu>
2018-03-19 21:35:09 -0400

    updated sast to fit our model currently

--- sast.ml
+++ sast.ml
@@ -1,16 +1,19 @@
 (* Semantically-checked Abstract Syntax Tree and functions for printing it *)
-
+(* mem2318 *)
 open Ast
 
 type sexpr = typ * sx
 and sx =
-    SLiteral of int
-  | SFliteral of string
+    SIntLit of int
   | SBoolLit of bool
-  | SId of string
+  | SNoteLit of sexpr * sexpr * sexpr
+  | SChordLit of sexpr list
+  | SSeqLit of sexpr list
+  | SStringLit of string
   | SBinop of sexpr * op * sexpr
-  | SUnop of uop * sexpr
-  | SAssign of string * sexpr
+  | SUnop of unop * sexpr
+  | SVariable of string
+  | SAsn of string * sexpr
   | SCall of string * sexpr list
   | SNoexpr
 
@@ -36,11 +39,10 @@ type sprogram = bind list * sfunc_decl list
 
 let rec string_of_sexpr (t, e) =
   "(" ^ string_of_typ t ^ " : " ^ (match e with
-    SLiteral(l) -> string_of_int l
+    SIntLit(l) -> string_of_int l
   | SBoolLit(true) -> "true"
   | SBoolLit(false) -> "false"
-  | SFliteral(l) -> l
-  | SId(s) -> s
+  | SVariable(s) -> s
   | SBinop(e1, o, e2) ->
       string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
   | SUnop(o, e) -> string_of_uop o ^ string_of_sexpr e
@@ -48,13 +50,21 @@ let rec string_of_sexpr (t, e) =
   | SCall(f, el) ->
       f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
   | SNoexpr -> ""
-				  ) ^ ")"				     
+				  ) ^ ")"
+  | SNoteLit(e1,e2,e3)-> "(" ^ string_of_sexpr e1 ^ "," ^ string_of_sexpr e2 ^ " | " ^ string_of_sexpr e3 ^ ")"
+  | SChordLit(e1)-> "Chord" (*fix*)
+  | SSeqLit(e1)->"Seq"(*fix*)
+  | SStringLit(e1)->e1 
+
 
 let rec string_of_sstmt = function
     SBlock(stmts) ->
       "{\n" ^ String.concat "" (List.map string_of_sstmt stmts) ^ "}\n"
   | SExpr(expr) -> string_of_sexpr expr ^ ";\n";
   | SReturn(expr) -> "return " ^ string_of_sexpr expr ^ ";\n";
+  | SPlay(expr) -> "play " ^ string_of_sexpr expr ^ ";\n";
+  | SFunc(expr) -> "func " ^ string_of_sexpr expr ^ ";\n";
+  | SPrint(expr) -> "print " ^ string_of_sexpr expr ^ ";\n";
   | SIf(e, s, SBlock([])) ->
       "if (" ^ string_of_sexpr e ^ ")\n" ^ string_of_sstmt s
   | SIf(e, s1, s2) ->  "if (" ^ string_of_sexpr e ^ ")\n" ^
