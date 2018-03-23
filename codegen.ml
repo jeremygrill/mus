3468f6a1470a0bb7a17382524d1c243570ad4bbb
Jeremy Grill <jeremy.grill@columbia.edu>
2018-03-23 16:28:31 -0400

    migrating for hello world

--- codegen.ml
+++ codegen.ml
@@ -26,7 +26,6 @@ let translate (_, functions) =
   (* Add types to the context so we can use them in our LLVM code *)
   let i32_t      = L.i32_type    context
   and i8_t       = L.i8_type     context 
-  and void_t     = L.void_type   context 
   (* Create an LLVM module -- this is a "container" into which we'll 
      generate actual code *)
   and the_module = L.create_module context "MicroC" in
@@ -34,7 +33,6 @@ let translate (_, functions) =
   (* Convert MicroC types to LLVM types *)
   let ltype_of_typ = function
       A.Int   -> i32_t
-    | A.Void  -> void_t
     | t -> raise (Failure ("Type " ^ A.string_of_typ t ^ " not implemented yet"))
   in
 
@@ -58,7 +56,7 @@ let translate (_, functions) =
     let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder in
     (* Generate LLVM code for a call to MicroC's "print" *)
     let rec expr builder ((_, e) : sexpr) = match e with
-  SLiteral i -> L.const_int i32_t i (* Generate a constant integer *)
+  SStringLit i -> L.const_int i32_t i (* Generate a constant integer *)
       | SCall ("print", [e]) -> (* Generate a call instruction *)
     L.build_call printf_func [| int_format_str ; (expr builder e) |]
       "printf" builder 
